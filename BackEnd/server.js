2737374acd8d0bfeb728992231cecb90e1592cde
Davi Paulino <78768199+DaviTaiyo@users.noreply.github.com>
2026-05-05 22:01:34 -0300

    lastest lesson
    this lesson i learn how connect backend with frontend

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,8 +2,38 @@ const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2/promise");
 
-//chamar o modulo db.js
+const db = require("./db");
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
+
+app.get("/usuarios", async (req, res) => {
+    try {
+        const [lista] = await db.query("SELECT * FROM usuarios");
+        res.json(lista);
+    } catch (erro) {
+        res.status(500).json(erro);
+    }
+});
+
+app.post("/cadastro", async (req, res) => {
+try {
+    const {nome, email, genero, senha} = req.body;
+
+    const [resultado] = await db.query(
+        "INSERT INTO usuarios (nome, email, genero, senha) values (?, ?, ?, ?)",
+         [nome, email, genero, senha]);
+
+         res.json({ id: resultado.insertId, nome, email, genero});
+} catch (erro) {
+ res.status(500).json(erro);
+}
+
+})
+
+app.listen(3000, () => {
+    console.log("API ESTA RODANDO NA PORTA: http://localhost:3000")
+})
--- FrontEnd/View/Registrar/script.js
+++ FrontEnd/View/Registrar/script.js
@@ -1,76 +1,99 @@
 $(document).ready(function () {
 
-    $(".register-container").fadeIn(800, function () {
-  
-      $(".campo").eq(0).slideDown(300, function () {
-        $(".campo").eq(1).slideDown(300, function () {
-          $(".campo").eq(2).slideDown(300, function () {
-            $(".campo").eq(3).slideDown(300, function () {
-              $("#btnCadastrar").fadeIn(400);
-            });
+  $(".register-container").fadeIn(800, function () {
+
+    $(".campo").eq(0).slideDown(300, function () {
+      $(".campo").eq(1).slideDown(300, function () {
+        $(".campo").eq(2).slideDown(300, function () {
+          $(".campo").eq(3).slideDown(300, function () {
+            $("#btnCadastrar").fadeIn(400);
           });
         });
       });
-  
     });
-  
-    $("#btnCadastrar").click(function () {
-      let nome = $("#nome").val().trim();
-      let email = $("#email").val().trim();
-      let senha = $("#senha").val().trim();
-      let confirmarSenha = $("#confirmarSenha").val().trim();
-  
-      $("#mensagem").hide();
-  
-      if (nome === "" || email === "" || senha === "" || confirmarSenha === "") {
-        mostrarErro("Preencha todos os campos!");
+
+  });
+
+  $("#btnCadastrar").click(async function () {
+    let nome = $("#nome").val().trim();
+    let email = $("#email").val().trim();
+    let senha = $("#senha").val().trim();
+    let genero = $("#generoSelect").val().trim();
+
+    $("#mensagem").hide();
+
+    if (nome === "" || email === "" || senha === "" || genero === "") {
+      mostrarErro("Preencha todos os campos!");
+      return;
+    }
+
+    if (!email.includes("@")) {
+      mostrarErro("Digite um e-mail válido!");
+      return;
+    }
+
+    if (senha.length < 6) {
+      mostrarErro("A senha deve ter pelo menos 6 caracteres!");
+      return;
+    }
+
+    try {
+      $("#btnCadastrar").text("Cadastrando...")
+
+      const resposta = await fetch("http://localhost:3000/cadastro", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          nome: nome,
+          email: email,
+          genero: genero,
+          senha: senha
+        })
+      });
+
+      if (!resposta.ok) {
+        mostrarErro("Erro ao cadastrar usuário");
+        $("#btnCadastrar").text("Cadastrar");
         return;
       }
-  
-      if (!email.includes("@")) {
-        mostrarErro("Digite um e-mail válido!");
-        return;
-      }
-  
-      if (senha.length < 6) {
-        mostrarErro("A senha deve ter pelo menos 6 caracteres!");
-        return;
-      }
-  
-      if (senha !== confirmarSenha) {
-        mostrarErro("As senhas não coincidem!");
-        return;
-      }
-  
+
       $("#mensagem")
         .removeClass("erro")
         .addClass("sucesso")
         .text("Cadastro realizado com sucesso!")
         .fadeIn(500);
-  
-      $("#btnCadastrar")
-        .text("Cadastrando...")
-        .slideUp(200)
-        .slideDown(200);
-    });
-  
-    function mostrarErro(texto) {
-      $("#mensagem")
-        .removeClass("sucesso")
-        .addClass("erro")
-        .text(texto)
-        .fadeIn(500);
-  
-      $(".register-container")
-        .animate({ marginLeft: "-10px" }, 100)
-        .animate({ marginLeft: "10px" }, 100)
-        .animate({ marginLeft: "-10px" }, 100)
-        .animate({ marginLeft: "10px" }, 100)
-        .animate({ marginLeft: "0px" }, 100);
+
+      setTimeout(function () {
+        window.location.href = "../Registrar/registrar.html"
+      }, 1000);
+
     }
-  
+    catch (erro) {
+      mostrarErro("Erro ao conectar com a API!");
+      $("#btnCadastrar").text("Cadastrar");
+    }
+
+    $("#btnCadastrar")
+      .text("Cadastrando...")
+      .slideUp(200)
+      .slideDown(200);
   });
 
-  function navRegistrar() {
-    window.location.href = "../home/homepage.html";
-}
+  function mostrarErro(texto) {
+    $("#mensagem")
+      .removeClass("sucesso")
+      .addClass("erro")
+      .text(texto)
+      .fadeIn(500);
+
+    $(".register-container")
+      .animate({ marginLeft: "-10px" }, 100)
+      .animate({ marginLeft: "10px" }, 100)
+      .animate({ marginLeft: "-10px" }, 100)
+      .animate({ marginLeft: "10px" }, 100)
+      .animate({ marginLeft: "0px" }, 100);
+  }
+
+});
